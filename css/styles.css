09c0437308168e65feb95745d24f8bf67409c4c9
bidkar <bidkar@gmail.com>
2017-01-17 15:52:55 -0700

    commit inicial

---
+++ css/styles.css
@@ -0,0 +1,16 @@
+/* Reglas CSS */
+/*Tipo etiqueta*/
+body,h1,h2,h3,h4,h5,h6{
+    margin: 0;
+    padding: 0;
+    border: 0;
+}
+/*Tipo ID*/
+#encabezado{
+    background-color: #93485E;
+    padding: 10px;
+    color: #000;
+    /*color: rgb(255, 255, 0);
+    color: rgb(255, 255, 0,.5);
+    color: white;*/
+}
