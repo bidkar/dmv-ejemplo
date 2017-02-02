f549ff2df802d7de9dbc188deaace2c72d7215bf
bidkar <bidkar@gmail.com>
2017-02-01 17:08:51 -0700

    Validacion JS
    Perfil de usuario con variables de session

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 function login(usuario, passwd) {
-    $.post("login.php", { txtusuario:usuario, txtpassword:passwd }, function(data) {
-        acceso = data.data.login;
+    $.post("login.php", { txtusuario:usuario, txtpassword:passwd }, function(response) {
+        var acceso = response.data.login;
         if (acceso == true) {
-            console.log('acceso correcto');
+            var usuario = response.data.usuario;
+            sessionStorage.userid = usuario.id;
+            sessionStorage.username = usuario.username;
+            sessionStorage.nombre = usuario.nombre;
+            sessionStorage.apellidos = usuario.apellidos;
+            sessionStorage.email = usuario.email;
+            console.log(usuario);
         } else if (acceso == false) {
+            $('#login-form-msg').html('Usuario o contraseña incorrecta');
+            $('#login-form-msg').removeClass('oculto');
+            $('#txtpassword').val('');
             console.warn('acceso incorrecto');
         } else {
+            $('#login-form-msg').html('Error en la conexion a la base de datos');
+            $('#login-form-msg').removeClass('oculto');
             console.error('error en la consulta');
         }
     });
@@ -16,6 +27,20 @@ $(document).ready(function() {
         event.preventDefault();
         var usuario = $('#txtusuario').val();
         var passwd = $('#txtpassword').val();
-        login(usuario, passwd);
+        if (usuario == '') {
+            // quitar la clase oculto del div de error de txtusuario
+            $('#txtusuario+div').removeClass('oculto');
+        } else {
+            $('#txtusuario+div').addClass('oculto');
+        }
+        if (passwd == '') {
+            // quitar la clase oculto del div de error de txtpassword
+            $('#txtpassword+div').removeClass('oculto');
+        } else {
+            $('#txtpassword+div').addClass('oculto');
+        }
+        if (usuario != '' && passwd != '') {
+            login(usuario, passwd);
+        }
     });
 });
